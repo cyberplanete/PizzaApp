d8d255bea890e7286500a195458e6abf35aae3fe
pascal <pascal.joret@gmail.com>
2020-05-12 05:24:53 -0700

    OnPropertyChanged

--- PizzaApp/Model/PizzaCell.cs
+++ PizzaApp/Model/PizzaCell.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
 using PizzaApp.extensions;
+using Xamarin.Forms;
 
 namespace PizzaApp
 {
-    public class PizzaCell
+    public class PizzaCell: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public Pizza pizza { get; set; }
 
         public bool IsFavorite { get; set; }
 
         public string ImageSourceFav { get { return IsFavorite ? "star2.png" : "star1.png"; } }
 
+        // depuis le xaml Source="{Binding ImageSourceFav}" Command="FavClickCommand"
+        public ICommand FavClickCommand { get; set; }
+
         public PizzaCell()
         {
+            FavClickCommand = new Command((obj) =>
+                {
+                    Console.WriteLine("FavClickCommand");
+                    //Si true alors faux, si faux alors vrai -- Permet d'alterner l'image
+                    IsFavorite = !IsFavorite;
+                    //Binding Xaml de ImageSourceFav -- Mise à jour de l'image favoris
+                    OnPropertyChanged("ImageSourceFav");
+                }
+            );
         }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged !=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        
     }
 }
